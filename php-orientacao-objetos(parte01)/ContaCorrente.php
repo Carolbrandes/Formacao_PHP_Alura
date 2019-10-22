45a6b5c9b7ebff399cf71a42c7306fbb6f6f0dd6
Carol <carolbrandes23@gmail.com>
2019-10-22 02:23:38 -0300

    aula 02 e 03

--- php-orientacao-objetos(parte01)/ContaCorrente.php
+++ php-orientacao-objetos(parte01)/ContaCorrente.php
@@ -1,16 +1,49 @@
 <?php
 
-class ContaCorrente{
-    public $titular;
-    public $agencia;
-    public $conta;
-    public $saldo;
+class ContaCorrente
+{
+  private $titular;
+  private $agencia;
+  private $conta;
+  private $saldo;
 
-    public function __construct($titular, $agencia, $conta, $saldo)
-    {
-      $this->titular = $titular;
-      $this->agencia = $agencia;
-      $this->conta = $conta;
-      $this->saldo = $saldo;  
+  public function __construct(string $titular, string $agencia, string $conta, float $saldo)
+  {
+    $this->titular = $titular;
+    $this->agencia = $agencia;
+    $this->conta = $conta;
+    $this->saldo = $saldo;
+  }
+
+  public function sacar(float $valor): void
+  {
+    $this->saldo -= $valor;
+  }
+
+  public function depositar(float $valor): void
+  {
+    $this->saldo += $valor;
+  }
+
+  // para acessar fora da classe: $conta1 -> propriedade
+  public function __get($propriedade)
+  {
+    return $this->$propriedade;
+
+    if($propriedade == "saldo"){
+      formataSaldo();
     }
+  }
+
+  public function __set($propriedade, $value)
+  {
+    if ($propriedade != "titular" && $propriedade != "saldo") {
+      $this->$propriedade = $valor;
+    }
+  }
+
+  private function formataSaldo()
+  {
+    return "R$ " . number_format($this->saldo, 2, ",", ".");
+  }
 }
--- php-orientacao-objetos(parte01)/index.php
+++ php-orientacao-objetos(parte01)/index.php
@@ -3,5 +3,10 @@
 require "ContaCorrente.php";
 
 $conta1 = new ContaCorrente("Maria", "1212", "343423-9", 6000.80);
+// $conta1->sacar(40);
+// // var_dump($conta1);
+// $conta1->depositar(50);
+// var_dump($conta1);
 
-var_dump($conta1);
+echo $conta1->titular;
+echo $conta1->saldo;
